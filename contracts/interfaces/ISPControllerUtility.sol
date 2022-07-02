4608b45be84eb695ee4420dfd4f43530805d2ce7
devmachine <brightsolutions4@outlook.com>
2022-07-02 16:30:21 -0400

    Refactor ISPToken contract

--- contracts/interfaces/ISPControllerUtility.sol
+++ contracts/interfaces/ISPControllerUtility.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
-import './IJBDirectory.sol';
+import './ISPDirectory.sol';
 
-interface IJBControllerUtility {
+interface ISPControllerUtility {
   function directory() external view returns (IJBDirectory);
 }
